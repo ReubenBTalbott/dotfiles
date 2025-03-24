e753edd5fcd5c86199d063bb195a7224613e6b5b
Reuben Talbott <rtalbott@spark451.com>
2025-03-24 10:35:19 -0400

    Add VScode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,21 @@ if status is-interactive
 end
 
 set tide_prompt_add_newline_before false
-source ~/.asdf/asdf.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 set -Ux COMPOSER_FUND 0
